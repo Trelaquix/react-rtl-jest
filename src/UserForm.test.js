ac5a5a063cf66673462b4815346a172783d1d625
Bryan <bryanlon@yahoo.com>
2023-03-20 23:26:15 +0800

    Update UserForm.test.js

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -3,11 +3,47 @@ import user from "@testing-library/user-event";
 import UserForm from "./UserForm";
 
 test("it shows two inputs and a button", () => {
+    // Render the component
     render(<UserForm />)
 
+    // Manipulate the component or find an element in it
     const inputs = screen.getAllByRole('textbox')
     const button = screen.getByRole('button')
 
+    // Assertion - make sure the component is doing what we expect it to do
     expect(inputs).toHaveLength(2)
     expect(button).toBeInTheDocument();
+});
+
+test("it calls onUserAdd when the form is submitted", () => {
+    // Not the best implementation
+    const argList = []
+    const callback = (...args) => {
+        argList.push(args)
+    };
+
+    // Render the component
+    render(<UserForm onUserAdd={callback} />)
+
+    // Find the two inputs
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+    // Stimulate typing in a name
+    user.click(nameInput);
+    user.keyboard('Bryan');
+
+    // Stimulate typing in an email
+    user.click(emailInput);
+    user.keyboard('bryan@bryan.com');
+
+    // Find the button
+    const button = screen.getByRole('button')
+
+    // Stimulate clicking the button
+    user.click(button);
+
+    // Assertion to make sure 'onUserAdd' gets called with email/name
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({ name: 'Bryan', email: 'bryan@bryan.com' });
+    // expect(callback).toHaveBeenCalled();
 });
