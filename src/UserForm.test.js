ef69eebc75d646d71c16e98f19758da163fa5b69
Bryan <bryanlon@yahoo.com>
2023-03-21 23:28:54 +0800

    Update UserForm

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -3,43 +3,51 @@ import user from "@testing-library/user-event";
 import UserForm from "./UserForm";
 
 test("it shows two inputs and a button", () => {
-    // Render the component
-    render(<UserForm />)
+  // Render the component
+  render(<UserForm />);
 
-    // Manipulate the component or find an element in it
-    const inputs = screen.getAllByRole('textbox')
-    const button = screen.getByRole('button')
+  // Manipulate the component or find an element in it
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
 
-    // Assertion - make sure the component is doing what we expect it to do
-    expect(inputs).toHaveLength(2)
-    expect(button).toBeInTheDocument();
+  // Assertion - make sure the component is doing what we expect it to do
+  expect(inputs).toHaveLength(2);
+  expect(button).toBeInTheDocument();
 });
 
 test("it calls onUserAdd when the form is submitted", () => {
-    // Not the best implementation
-    const mock = jest.fn()
+  // Not the best implementation
+  const mock = jest.fn();
 
-    // Render the component
-    render(<UserForm onUserAdd={mock} />)
+  // Render the component
+  render(<UserForm onUserAdd={mock} />);
 
-    // Find the two inputs
-    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  // Find the two inputs
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
 
-    // Stimulate typing in a name
-    user.click(nameInput);
-    user.keyboard('Bryan');
+  // Stimulate typing in a name
+  user.click(nameInput);
+  user.keyboard("Bryan");
 
-    // Stimulate typing in an email
-    user.click(emailInput);
-    user.keyboard('bryan@bryan.com');
+  // Stimulate typing in an email
+  user.click(emailInput);
+  user.keyboard("bryan@bryan.com");
 
-    // Find the button
-    const button = screen.getByRole('button')
+  // Find the button
+  const button = screen.getByRole("button");
 
-    // Stimulate clicking the button
-    user.click(button);
+  // Stimulate clicking the button
+  user.click(button);
 
-    // Assertion to make sure 'onUserAdd' gets called with email/name
-    expect(mock).toHaveBeenCalled()
-    expect(mock).toHaveBeenCalledWith({ name: 'Bryan', email: 'bryan@bryan.com' })
+  // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({
+    name: "Bryan",
+    email: "bryan@bryan.com",
+  });
 });
